ebca992dc8e3139fd841f2e940c3607da7ad0556
Lia Pires <liasth.pires@gmail.com>
2022-07-25 15:59:58 -0300

    create inverter-string.py

---
+++ inverter-string.py
@@ -0,0 +1,14 @@
+string = "bolo"
+string2 = "casa"
+string3 = "azul"
+
+
+
+
+def inverter_string(string):
+    return string[::-1]
+
+
+print(inverter_string(string))
+print(inverter_string(string2))
+print(inverter_string(string3))
